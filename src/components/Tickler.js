e5e2446de0e009fd28955e0d1c37544744dba869
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-21 20:18:41 -0600

    commit tree 9eab3329d895e84dd851c91154d0a4bd06ef8219

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 function Tickler() {
-  function tickle(event) {
-    console.log(event);
-  }
+  return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>;
+} 
 
-  return <button onClick={tickle}>Tickle me!</button>;
-}
+ 
 
 export default Tickler;
